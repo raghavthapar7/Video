98707087092d0cad92046921faa42aa36e527758
Raghav Thapar <raghavthapar.apps@gmail.com>
2023-09-20 15:45:40 +0530

    Added the button transition via JS

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
 "use strict";
+
+// VARIABLES TO BE USED
+
+let app = document.querySelector(".app");
+let video = document.querySelector(".video-container");
+let btnSwitch = document.querySelector(".switch-btn");
+let switchOverlay = document.querySelector(".switch");
+
+btnSwitch.addEventListener("click", () => {
+  if (switchOverlay.classList.contains("play")) {
+    switchOverlay.classList.add("pause");
+    switchOverlay.classList.remove("play");
+  } else {
+    switchOverlay.classList.add("play");
+    switchOverlay.classList.remove("pause");
+  }
+});
